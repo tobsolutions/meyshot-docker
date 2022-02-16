7f7b1e880ffb598865a0c3031871ce5c0e693bcf
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2022-02-16 20:37:56 +0100

    sql

--- cronmaster/1min.sh
+++ cronmaster/1min.sh
@@ -3,18 +3,21 @@ echo "- `date -u` 1min.sh" >> /proc/1/fd/1
 ssmdb2_host=h2790537.stratoserver.net
 ssmdb2_user=ssmdb2
 ssmdb2_password=mc4hct
-ssmdb2_db=ssmdb2
+ssmdb2_db=SSMDB2
 echo "Dump wird aus SSMDB2-Datenbank erstellt.."
-echo "mysqldump -hssmdb2 -uroot -pmc4hct ssmdb2 > /usr/local/bin/ssmdb2.sql"
-mysqldump -hssmdb2 -uroot -pmc4hct ssmdb2 > /usr/local/bin/ssmdb2.sql
+echo "mysqldump -hsqldb -uroot -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql"
+mysqldump -hsqldb -uroot -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql
 echo "Dump erstellt."
 
 #echo "Dump wird aus SSMDB2-Datenbank erstellt..";
-#echo "mysqldump -h192.168.10.200 -umeyton -pmc4hct SSMDB2 > ssmdb2.sql";
+#mysqldump -h192.168.10.200 -umeyton -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql
 #echo "Dump erstellt.";
+#echo "Dump wird lokal eingespielt..";
+#mysql -hsqldb -uroot -pmc4hct SSMDB2 -f < /usr/local/bin/SSMDB2.sql
+#echo "Dump eingespielt.";
 #echo "Dump wird auf Webserver eingespielt..";
-#echo "mysql -h$ssmdb2_host -u$ssmdb2_user -p$ssmdb2_password $ssmdb2_db -f < ssmdb2.sql";
+#mysql -h$ssmdb2_host -u$ssmdb2_user -p$ssmdb2_password $ssmdb2_db -f < /usr/local/bin/SSMDB2.sql";
 #echo "Dump eingespielt.";
 #echo "Setze Uploaddatum..";
-#mysql -h$ssmdb2_host -u$ssmdb2_user -p$ssmdb2_password $ssmdb2_db -e "INSER INTO Upload (Uploaddatum) VALUES (NULL)"
+#mysql -h$ssmdb2_host -u$ssmdb2_user -p$ssmdb2_password $ssmdb2_db -e "INSERT INTO `Upload` (`Uploaddatum`) VALUES (CURRENT_TIMESTAMP)"
 #echo "Fertig.";
--- sqldb/startup.sql
+++ sqldb/startup.sql
@@ -2,6 +2,12 @@
 CREATE DATABASE IF NOT EXISTS `MEYSHOT`;
 CREATE DATABASE IF NOT EXISTS `SSMDB2`;
 
+# create tables
+CREATE TABLE `MEYSHOT`.`Upload` ( `Uploaddatum` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`Uploaddatum`)) ENGINE = InnoDB;
+
 # create root user and grant rights
 CREATE USER 'otto'@'%' IDENTIFIED BY 'otto';
 GRANT ALL PRIVILEGES ON *.* TO 'otto'@'%';
+
+# set timezone
+SET GLOBAL time_zone = 'Europe/Berlin';
