bdef93352cf44c3f5e3246add34e4d1f8ed9a632
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2022-02-16 20:13:15 +0100

    Update startup.sql

--- sqldb/startup.sql
+++ sqldb/startup.sql
@@ -1,3 +1,8 @@
 # create databases
-CREATE DATABASE IF NOT EXISTS `aufsichten`;
-CREATE DATABASE IF NOT EXISTS `upload`;
+CREATE DATABASE IF NOT EXISTS `_aufsichten`;
+CREATE DATABASE IF NOT EXISTS `_upload`;
+CREATE DATABASE IF NOT EXISTS `_SSMDB2`;
+
+# create root user and grant rights
+CREATE USER 'otto'@'localhost' IDENTIFIED BY 'otto';
+GRANT ALL PRIVILEGES ON *.* TO 'otto'@'%';
