29ed995eb44adf0ccc30b8493437d836df108b7c
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2022-02-21 20:40:41 +0100

    Update 1min.sh

--- cronmaster/1min.sh
+++ cronmaster/1min.sh
@@ -7,19 +7,19 @@ ssmdb2_db=SSMDB2
 #echo "Dump wird aus SSMDB2-Datenbank erstellt.."
 #echo "mysqldump -hsqldb -uroot -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql"
 #mysqldump -hsqldb -uroot -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql
-#echo "Dump erstellt."
 
 echo "Dump wird aus SSMDB2-Datenbank erstellt..";
 echo "mysqldump -h192.168.10.200 -umeyton -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql";
 mysqldump -h192.168.10.200 -umeyton -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql
-echo "Dump erstellt.";
 echo "Dump wird lokal eingespielt..";
 echo "mysql -hsqldb -uroot -pmc4hct SSMDB2 -f < /usr/local/bin/SSMDB2.sql";
 mysql -hsqldb -uroot -pmc4hct SSMDB2 -f < /usr/local/bin/SSMDB2.sql
-echo "Dump eingespielt.";
+echo "Setze lokales Uploaddatum..";
+echo "mysql --hsqldb -uroot -pmc4hct MEYSHOT -e 'INSERT INTO `Upload` (`Uploaddatum`) VALUES (CURRENT_TIMESTAMP)'";
+mysql --hsqldb -uroot -pmc4hct MEYSHOT -e "INSERT INTO `Upload` (`Uploaddatum`) VALUES (CURRENT_TIMESTAMP)"
+#echo "Fertig.";
 #echo "Dump wird auf Webserver eingespielt..";
 #mysql -h$ssmdb2_host -u$ssmdb2_user -p$ssmdb2_password $ssmdb2_db -f < /usr/local/bin/SSMDB2.sql";
-#echo "Dump eingespielt.";
 #echo "Setze Uploaddatum..";
 #mysql -h$ssmdb2_host -u$ssmdb2_user -p$ssmdb2_password $ssmdb2_db -e "INSERT INTO `Upload` (`Uploaddatum`) VALUES (CURRENT_TIMESTAMP)"
-#echo "Fertig.";
+echo "Fertig.";
