861bab0a3e2261b3859b4c22ca7752a5ff4dfb9b
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2022-02-21 22:07:10 +0100

    update cron

--- cronmaster/5min.sh
+++ cronmaster/5min.sh
@@ -1,1 +1,30 @@
 echo "- `date -u` 5min.sh" >> /proc/1/fd/1
+
+ssmdb2_host=h2790537.stratoserver.net
+ssmdb2_user=meyshot
+ssmdb2_password=*2Aj72zx
+ssmdb2_db=SSMDB2
+ssmdb2_meyshot=MEYSHOT
+#echo "Dump wird aus SSMDB2-Datenbank erstellt.."
+#echo "mysqldump -hsqldb -uroot -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql"
+#mysqldump -hsqldb -uroot -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql
+
+echo "- `date -u` Dump wird aus SSMDB2-Datenbank erstellt..";
+echo "- `date -u` mysqldump -h192.168.10.200 -umeyton -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql";
+mysqldump -h192.168.10.200 -umeyton -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql
+echo "- `date -u` Dump wird lokal eingespielt..";
+echo "- `date -u` mysql -hsqldb -uroot -pmc4hct SSMDB2 -f < /usr/local/bin/SSMDB2.sql";
+mysql -hsqldb -uroot -pmc4hct SSMDB2 -f < /usr/local/bin/SSMDB2.sql
+echo "- `date -u` Setze lokales Uploaddatum..";
+echo "- `date -u` mysql -hsqldb -uroot -pmc4hct MEYSHOT -e 'INSERT INTO Upload (Uploaddatum) VALUES (CURRENT_TIMESTAMP)'";
+mysql -hsqldb -uroot -pmc4hct MEYSHOT -e 'INSERT INTO `Upload` (`Uploaddatum`) VALUES (CURRENT_TIMESTAMP)'
+echo "- `date -u` Dump wird aus MEYSHOT-Datenbank erstellt..";
+echo "- `date -u` mysqldump -hsqldb -uroot -pmc4hct MEYSHOT > /usr/local/bin/MEYSHOT.sql";
+mysqldump -hsqldb -uroot -pmc4hct MEYSHOT > /usr/local/bin/MEYSHOT.sql
+echo "- `date -u` Dump für SSMDB2 wird auf Webserver eingespielt..";
+echo "- `date -u` mysql -h$ssmdb2_host -u$ssmdb2_user -p$ssmdb2_password $ssmdb2_db -f < /usr/local/bin/SSMDB2.sql";
+mysql -h$ssmdb2_host -u$ssmdb2_user -p$ssmdb2_password $ssmdb2_db -f < /usr/local/bin/SSMDB2.sql
+echo "- `date -u` Dump für MEYSHOT wird auf Webserver eingespielt..";
+echo "- `date -u` mysql -h$ssmdb2_host -u$ssmdb2_user -p$ssmdb2_password $ssmdb2_meyshot -f < /usr/local/bin/MEYSHOT.sql";
+mysql -h$ssmdb2_host -u$ssmdb2_user -p$ssmdb2_password $ssmdb2_meyshot -f < /usr/local/bin/MEYSHOT.sql
+echo "- `date -u` Fertig.";
