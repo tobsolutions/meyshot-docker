131556b0d19de9c1a0b97cfa0f87ca82c6bd8900
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2022-03-29 20:12:21 +0200

    vis

--- www/vis/dbconnect.php
+++ www/vis/dbconnect.php
@@ -1,3 +1,14 @@
 <?php
-$link_meyshot = mysqli_connect('sqldb', 'root', 'mc4hct', 'MEYSHOT');
+$host='sqldb';
+$user='root';
+$pass='mc4hct';
+
+$link_meyshot = mysqli_connect($host, $user, $pass, 'MEYSHOT');
+
+$conn = new mysqli($host, $user, $pass);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
 ?>
