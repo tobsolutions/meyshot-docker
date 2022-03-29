ef04cc6b287dd2a197c6b49183c6a910b3487cf1
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2022-03-29 20:16:26 +0200

    Update dbconnect.php

--- www/vis/dbconnect.php
+++ www/vis/dbconnect.php
@@ -5,10 +5,7 @@ $pass='mc4hct';
 
 $link_meyshot = mysqli_connect($host, $user, $pass, 'MEYSHOT');
 
-$conn = new mysqli($host, $user, $pass);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL server successfully!";
+if(!$link_meyshot) {
+    die('failed to connect to the server: ' . mysqli_connect_error());
 }
 ?>
