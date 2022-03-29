ac0fd7f1cdb993c66c17fd3a2b90b09964c44d87
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2022-03-29 20:20:56 +0200

    Update index.php

--- www/vis/index.php
+++ www/vis/index.php
@@ -16,7 +16,7 @@
 </head>
 
 <body>
-  <?php require('dbconnect2.php'); ?>
+  <?php require('dbconnect.php'); ?>
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
     <div class="container">
