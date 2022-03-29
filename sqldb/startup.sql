c8d4bfa3f0bc71d1f61c732232e34aa4d8828eaa
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2022-03-29 19:59:52 +0200

    vis

--- sqldb/startup.sql
+++ sqldb/startup.sql
@@ -4,6 +4,7 @@ CREATE DATABASE IF NOT EXISTS `SSMDB2`;
 
 # create tables
 CREATE TABLE `MEYSHOT`.`Upload` ( `Uploaddatum` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`Uploaddatum`)) ENGINE = InnoDB;
+CREATE TABLE `MEYSHOT`.`Infoticker` ( `ID` INT NOT NULL AUTO_INCREMENT , `Startdatum` TIMESTAMP NULL , `Enddatum` TIMESTAMP NULL , `Titel` TEXT NOT NULL , `Text` TEXT NOT NULL , `Ersteller` TEXT NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB;
 
 # create root user and grant rights
 CREATE USER 'otto'@'%' IDENTIFIED BY 'otto';
--- www/vis/index.php
+++ www/vis/index.php
@@ -1,3 +1,4 @@
+<?php require_once('dbconnect.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 
