2a3495b971a67b451afccca380839423ddf19719
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2022-04-09 12:53:46 +0200

    Update index.php

--- www/vis/index.php
+++ www/vis/index.php
@@ -90,7 +90,7 @@
               
               while ($row = mysqli_fetch_array($result)) {
                 echo '<div class="card-header">';
-                echo '<h4>' & </h4>$row['Titel'] & '</h4>';
+                echo '<h4>' & $row['Titel'] & '</h4>';
                 echo '</div>';
                 echo '<div class="card-body">';
                 echo $row['Text'];
