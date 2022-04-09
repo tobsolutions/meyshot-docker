2a80780a373bf4e87e2da3591e395be358b07468
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2022-04-09 12:44:09 +0200

    Update index.php

--- www/vis/index.php
+++ www/vis/index.php
@@ -89,7 +89,7 @@
                 $result = mysqli_query($link_meyshot, $sql);
                 
                 while ($row = mysqli_fetch_array($result)) {
-                    echo $row['Titel'] & $row['Text'] & $row[2] & $row;
+                    echo $row['Titel'] & $row['Text'];
                 }
 
                 mysqli_free_result($result);
