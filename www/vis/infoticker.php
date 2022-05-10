1a35669d453842423d7c75f5c829ab26aa8ccef5
Tobias Sailer <tobi@tobsolutions.de>
2022-05-10 21:19:02 +0200

    index

---
+++ www/vis/infoticker.php
@@ -0,0 +1,22 @@
+<?php
+require('dbconnect.php');
+$sql = 'SELECT * FROM Infoticker WHERE Enddatum >= NOW() AND Startdatum <= NOW() ORDER BY Startdatum ASC';
+$result = mysqli_query($link_meyshot, $sql);
+if ( ! $result )
+{
+    die('Ungültige Abfrage: ' . mysqli_error());
+}
+
+while ($row = mysqli_fetch_array($result)) {
+    echo '<div class="card" width="100%">';
+    echo '<div class="card-header">';
+    echo '<h4>' . $row['Titel'] . '</h4>';
+    echo '</div>';
+    echo '<div class="card-body">';
+    echo $row['Text'];
+    echo '</div>';
+    echo '</div>';
+}
+
+mysqli_free_result($result);
+?>
