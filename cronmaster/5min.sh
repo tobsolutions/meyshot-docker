eed7cc9322dc53f7981075f6a07f292db0846aed
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2022-09-27 20:37:43 +0200

    Update 5min.sh

--- cronmaster/5min.sh
+++ cronmaster/5min.sh
@@ -9,6 +9,7 @@ ssmdb2_meyshot=MEYSHOT
 #echo "mysqldump -hsqldb -uroot -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql"
 #mysqldump -hsqldb -uroot -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql
 
+echo "- `date -u` ### SSMDB2 MEYSHOT Upload ###";
 echo "- `date -u` Dump wird aus SSMDB2-Datenbank erstellt..";
 echo "- `date -u` mysqldump -h192.168.10.200 -umeyton -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql";
 mysqldump -h192.168.10.200 -umeyton -pmc4hct SSMDB2 > /usr/local/bin/SSMDB2.sql
@@ -16,7 +17,7 @@ echo "- `date -u` Dump wird lokal eingespielt..";
 echo "- `date -u` mysql -hsqldb -uroot -pmc4hct SSMDB2 -f < /usr/local/bin/SSMDB2.sql";
 mysql -hsqldb -uroot -pmc4hct SSMDB2 -f < /usr/local/bin/SSMDB2.sql
 echo "- `date -u` Setze lokales Uploaddatum..";
-echo "- `date -u` mysql -hsqldb -uroot -pmc4hct MEYSHOT -e 'INSERT INTO Upload (Uploaddatum) VALUES (CURRENT_TIMESTAMP)'";
+echo "- `date -u` mysql -hsqldb -uroot -pmc4hct MEYSHOT -e 'INSERT INTO 'Upload' ('Uploaddatum') VALUES (CURRENT_TIMESTAMP)'";
 mysql -hsqldb -uroot -pmc4hct MEYSHOT -e 'INSERT INTO `Upload` (`Uploaddatum`) VALUES (CURRENT_TIMESTAMP)'
 echo "- `date -u` Dump wird aus MEYSHOT-Datenbank erstellt..";
 echo "- `date -u` mysqldump -hsqldb -uroot -pmc4hct MEYSHOT > /usr/local/bin/MEYSHOT.sql";
@@ -28,3 +29,10 @@ echo "- `date -u` Dump für MEYSHOT wird auf Webserver eingespielt..";
 echo "- `date -u` mysql -h$ssmdb2_host -u$ssmdb2_user -p$ssmdb2_password $ssmdb2_meyshot -f < /usr/local/bin/MEYSHOT.sql";
 mysql -h$ssmdb2_host -u$ssmdb2_user -p$ssmdb2_password $ssmdb2_meyshot -f < /usr/local/bin/MEYSHOT.sql
 echo "- `date -u` Fertig.";
+
+echo "- `date -u` ### SMB FTP Upload ###";
+mkdir -p html
+cd html
+echo "- `date -u` Daten von SMB-Freigabe 'html' wird heruntergeladen";
+smbclient //192.168.10.200/html -N -c 'cd ./html; prompt; mget *'
+cd ..
