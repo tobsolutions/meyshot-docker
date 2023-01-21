b08f96f2f250780267caab3239506867b778ad3d
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2023-01-21 20:42:32 +0100

    Update dbconnect.php

--- www/dbconnect.php
+++ www/dbconnect.php
@@ -4,4 +4,5 @@ $user_server='meyshot';
 $pass_server='*2Aj72zx';
 
 $link_meyshot_server = mysqli_connect($host_server, $user_server, $pass_server, 'MEYSHOT');
+mysqli_set_charset($link_meyshot_server, "utf8");
 ?>
