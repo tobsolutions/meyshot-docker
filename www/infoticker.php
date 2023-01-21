2d729cebdbf7bb70e89858e72ccf3e2cbcdef064
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2023-01-21 20:32:23 +0100

    Update infoticker.php

--- www/infoticker.php
+++ www/infoticker.php
@@ -7,19 +7,21 @@
     <h1 class="h3 mb-0 text-gray-800">Infoticker</h1>
   </div>
 
+    <table class="table table-striped">
+        <th>
+            <td>Titel</td>
+            <td>Text</td>
+            <td>Startdatum</td>
+            <td>Enddatum</td>
+            <td>Ersteller</td>
+        </th>
+    </table>
+
   <div class="card shadow mb-4">
     <div class="card-header py-3">
       <h6 class="m-0 font-weight-bold text-primary">Hallo Welt!</h6>
     </div>
-    <table class="table table-striped">
-      <th>
-        <td>Titel</td>
-        <td>Text</td>
-        <td>Startdatum</td>
-        <td>Enddatum</td>
-        <td>Ersteller</td>
-      </tr>
-    </table>
+    
   </div>
 
 </div>
