fab120adffdc07e79d46697aabf107ec604bb568
tobsolutions <tobi@tobsolutions.de>
2023-02-07 21:53:08 +0100

    Update aufsicht.php

--- www/vis/aufsicht.php
+++ www/vis/aufsicht.php
@@ -4,7 +4,11 @@ $sql = 'SELECT `User`.`Name` FROM `Aufsichten`,`User` WHERE `User`.ID=Aufsichten
 $result = mysqli_query($link_meyshot, $sql);
 
 while ($row = mysqli_fetch_array($result)) {
-    echo $row[0];
+    if($row[0] == ""){
+        echo "keine Aufsicht angemeldet - siehe Aufsichtenplan"
+    } else {
+        echo $row[0];
+    }
 }
 
 mysqli_free_result($result);
