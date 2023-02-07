42662b28a513231203934e86715c8acec4e68921
tobsolutions <tobi@tobsolutions.de>
2023-02-07 21:45:34 +0100

    aufsicht

--- www/vis/index.php
+++ www/vis/index.php
@@ -21,9 +21,9 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
     <div class="container">
       <span class='nav-item'>aktuelle Aufsicht: siehe Aufsichtenplan
-      <?php /*
+      <?php
         include('dbconnect.php');
-        $sql = 'SELECT aufsichten_name FROM aufsichten_log, aufsichten WHERE aufsichten_log.aufsichten_id=aufsichten.aufsichten_id ORDER BY `aufsichten_log_login` ASC LIMIT 1';
+        $sql = 'SELECT `User`.`Name` FROM `Aufsichten`,`User` WHERE `User`.ID=Aufsichten.User AND `Ende` IS NULL';
         $result = mysqli_query($link_meyshot, $sql);
         
         while ($row = mysqli_fetch_array($result)) {
@@ -31,11 +31,8 @@
         }
 
         mysqli_free_result($result);
-      */?>
+      ?>
       </span>
-      <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>-->
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
@@ -48,10 +45,6 @@
 
   <!-- Page Content -->
   <div class="container-fluid">
-    <div class="alert alert-warning alert-dismissible" id="nosocket">
-      Es kann keine Verbindung zum Websocket ws://192.168.10.200:49472/ hergestellt werden.
-    </div>
-
     <div class="row">
       <div class="col-sm-9">
         <div class="row">
