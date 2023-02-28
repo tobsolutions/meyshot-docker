1793bf2233dad5e8e72079226886330e98422558
tobsolutions <tobi@tobsolutions.de>
2023-02-28 18:46:41 +0100

    Update dbconnect.php

--- www/vis/dbconnect.php
+++ www/vis/dbconnect.php
@@ -1,7 +1,10 @@
 <?php
-$host='sqldb';
-$user='root';
-$pass='mc4hct';
+//$host='sqldb';
+//$user='root';
+//$pass='mc4hct';
+$host='h2790537.stratoserver.net';
+$user='meyshot';
+$pass='*2Aj72zx';
 
 $link_meyshot = mysqli_connect($host, $user, $pass, 'MEYSHOT');
 ?>
