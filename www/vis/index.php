c0c2baaeb41e7dde2b1daca114b9aaa3ba546808
tobsolutions <tobi@tobsolutions.de>
2023-03-03 16:22:00 +0100

    vis

--- www/vis/index.php
+++ www/vis/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MeyShot VIS</title>
     <link href="vendor/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/css/carousel.css" rel="stylesheet">
 
     <style>
       body {
@@ -74,6 +75,10 @@
       .card{
         margin-bottom: 5px;
       }
+
+      .py-3{
+        padding-bottom: 0px !important;
+      }
     </style>
     <!-- Custom styles for this template -->
   </head>
@@ -91,8 +96,7 @@
         </div>
         <div class="row" id="content">
           <div class="col-9">
-            <img style="width: 100%; height: auto;" src="http://192.168.10.222:8090/?action=stream" />
-            <!--<img style="width: 100%; height: auto;" src="stream.jpg" />-->
+            <img id="stream" style="width: 100%; height: auto;" src="http://192.168.10.222:8090/?action=stream" />
           </div>
           <div class="col-3" id="infoticker">
             <div class="card">
@@ -106,30 +110,144 @@
       </div>
     </main>
 
-    <footer class="footer mt-auto py-3 bg-light">
+    <footer class="footer mt-auto bg-light" style="padding-top: 10px;">
       <div class="container-fluid">
-        <div class="row">
-          <class class="col-2">
-            <img src="logo.png" style="height:50px">
-          </class>
+        <div class="row align-items-end">
+          <div class="col-1">
+            <img src="img/logo.png" style="width:100%;">
+          </div>
+          <div class="col-1">
+            <img src="img/schuetzafest.png" style="width:100%;">
+          </div>
+          <div class="col-3" style="padding-bottom: 10px;">
+            <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-indicators" style="display:none;">
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="7" aria-label="Slide 8"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="8" aria-label="Slide 9"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="9" aria-label="Slide 10"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="10" aria-label="Slide 11"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="11" aria-label="Slide 12"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="12" aria-label="Slide 13"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="13" aria-label="Slide 14"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="14" aria-label="Slide 15"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="15" aria-label="Slide 16"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="16" aria-label="Slide 17"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="17" aria-label="Slide 18"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="18" aria-label="Slide 19"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Gold Ochsen</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Frankonia</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Gamper Holzbau</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Dietenbronner</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Kopanos - Gössler</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Zum Griaswirt - Schützenheim Vöhringen</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Foddis Technische Kunststoffteile</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Getränke Maier</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>MRX Performance</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Freizeitbad Nautilla</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Fahrrad Böttcher</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Handyman</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Ums Eck - Dana Neuhäusler </strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Brunnen Apotheke Bellenberg</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>beam GmbH</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Salmanbau</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Karger</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Metzgerei Maucher</strong>
+                </div>
+                <div class="carousel-item">
+                  <span>Mit freundlicher Unterstützung</br> </span>
+                  <strong>Stefan Joser Holzbau</strong>
+                </div>
+              </div>
+            </div>
+            <script src="vendor/js/bootstrap.bundle.min.js"></script>
+          </div>
+          <div class="col-7" style="padding-bottom: 10px;">
+            <img src="img/hauptsponsoren.png" width="100%">
+          </div>
         </div>
       </div>
     </footer>
-
+    
     <!-- JS -->
     <script src="js/jquery-3.4.1.js"></script>
     <script>
       $(document).ready(function() {
+        if(window.location.pathname.split(".")[1] == "html"){
+          document.getElementById("stream").src = "stream.jpg";
+        } else {
           refresh();
           setInterval(function() {
           refresh();
           }, 10000);
+        }
       });
   
       function refresh() {
-          $("#infoticker").load("infoticker.php");
-          $("#aufsicht").load("aufsicht.php");
-      }
-      </script>
+        $("#infoticker").load("infoticker.php");
+        $("#aufsicht").load("aufsicht.php");
+      }      
+    </script>
   </body>
 </html>
