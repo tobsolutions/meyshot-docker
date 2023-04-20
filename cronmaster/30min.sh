d4e8612dcc1018dadf0d8a105e99a40eeb6d826f
tobsolutions <tobi@tobsolutions.de>
2023-04-20 15:36:55 +0200

    cronmaster

--- cronmaster/30min.sh
+++ cronmaster/30min.sh
@@ -2,11 +2,11 @@ echo "- `date -u` 30min.sh job gestartet" >> /proc/1/fd/1
 
 #SMB FTP Upload
 echo "- `date -u` ### SMB FTP Upload ###";
-echo "- `date -u` Daten von SMB-Freigabe werden heruntergeladen";
+echo "- `date -u` PDF Daten von SMB-Freigabe werden heruntergeladen";
 cd /usr/local/bin/ergebnis/PDF;
 rm -rf *;
 smbclient //192.168.10.200/ergebnis -N -c 'prompt; cd PDF; recurse; mget *'
 cd ..
 
-echo "- `date -u` Daten werden auf FTP-Server hochgeladen";
+echo "- `date -u` PDF Daten werden auf FTP-Server hochgeladen";
 ncftpput -R -v -u "svtieba" -p "IkOowINN82M%6e7af" h2790537.stratoserver.net /meytonsmb /usr/local/bin/ergebnis
