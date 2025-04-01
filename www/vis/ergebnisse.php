eb9a02f43108eb88e79a87d30f1b7d88c617f7d4
tobsolutions <tobi@tobsolutions.de>
2025-04-01 19:47:36 +0200

    Update ergebnisse.php

--- www/vis/ergebnisse.php
+++ www/vis/ergebnisse.php
@@ -9,7 +9,7 @@ if ( ! $result )
 
 echo '<div class="card">';
 echo '<div class="card-body">';
-echo '<h5 class="card-title">Ergebnisse des Tages</h5>';
+echo '<h5 class="card-title">Ergebnisse des Tages</h5>' . date_create("28-03-2025");
 echo '<p class="card-text">';
 while ($row = mysqli_fetch_array($result)) {
     echo '<b>' . $row['Nachname'] . ' ' . $row['Vorname'] . '</b> ' . $row['TotalRing']/10 . ' (' . $row['TotalRing01']/10 . ')</br>';
