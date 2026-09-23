b1e54d800f2c3faa9e3ac55801d640acccc876b5
tobsolutions <60960030+tobsolutions@users.noreply.github.com>
2026-09-23 21:36:25 +0200

    Update ergebnisse.php

--- www/vis/ergebnisse.php
+++ www/vis/ergebnisse.php
@@ -10,7 +10,7 @@ if ( ! $result )
 echo '<div class="card">';
 echo '<div class="card-body">';
 echo '<h5 class="card-title">Ergebnisse des Tages</h5>';
-echo '<p class="card-text">';
+echo '<p id="ergebniscontent" class="card-text">';
 while ($row = mysqli_fetch_array($result)) {
     echo '<b>' . $row['Nachname'] . ' ' . $row['Vorname'] . '</b> ' . $row['TotalRing']/10 . ' (' . $row['TotalRing01']/10 . ')</br>';
 
